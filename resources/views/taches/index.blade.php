a996edb052267578dd0f5c12cddba0cc66c358f4
lamiaehajib <haynahayna421@gmail.com>
2025-07-01 16:20:53 +0100

    tsss

--- resources/views/taches/index.blade.php
+++ resources/views/taches/index.blade.php
@@ -112,7 +112,7 @@
 
                 @if (session('error'))
                     <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-lg relative mb-4 shadow-md animate-fade-in animate-pulse-subtle" role="alert">
-                        <strong class="font-bold">Erreur!</strong>
+                        <strong class="font-bold">Error!</strong>
                         <span class="block sm:inline">{{ session('error') }}</span>
                         <span class="absolute top-0 bottom-0 right-0 px-4 py-3 cursor-pointer" onclick="this.parentElement.style.display='none';">
                             <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Fermer</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 2.65a1.2 1.2 0 1 1-1.697-1.697L8.303 10l-2.651-2.651a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-2.651a1.2 1.2 0 1 1 1.697 1.697L11.697 10l2.651 2.651a1.2 1.2 0 0 1 0 1.698z"/></svg>
